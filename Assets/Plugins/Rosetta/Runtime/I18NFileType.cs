3e3cd1f34ecc687e38833af92e5fed7aa45c8ca9
molingyu <z1522716486@hotmail.com>
2019-04-19 15:56:48 +0800

    feat: add i18n file type enum

---
+++ Assets/Plugins/Rosetta/Runtime/I18NFileType.cs
@@ -0,0 +1,20 @@
+﻿namespace Rosetta.Runtime
+{
+    /// <summary>
+    ///     I18N file type. Each file type has a corresponding loader.
+    /// </summary>
+    public enum I18NFileType
+    {
+        Po,
+        Png,
+        Wav,
+        Font,
+
+        // TODO: Waiting for support
+        TMPFont,
+        Mo,
+        Json,
+        Xml,
+        Csv
+    }
+}
