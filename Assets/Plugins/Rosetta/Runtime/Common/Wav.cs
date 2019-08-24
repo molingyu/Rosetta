541f0b87060f4f67f0f7ced0bc9d1e7fefe58c2e
molingyu <z1522716486@hotmail.com>
2019-08-24 10:50:18 +0800

    feat: add bin2wav
     http://answers.unity3d.com/questions/737002/wav-byte-to-audioclip.html

---
+++ Assets/Plugins/Rosetta/Runtime/Common/Wav.cs
@@ -0,0 +1,96 @@
+using System.IO;
+
+namespace Rosetta.Runtime.Common
+{
+    /* From http://answers.unity3d.com/questions/737002/wav-byte-to-audioclip.html */
+    /* Author Jeff-Kesselman */
+    public class Wav
+    {
+        // Returns left and right double arrays. 'right' will be null if sound is mono.
+        public Wav(string filename) :
+            this(GetBytes(filename))
+        {
+        }
+
+        public Wav(byte[] wav)
+        {
+            // Determine if mono or stereo
+            ChannelCount = wav[22]; // Forget byte 23 as 99.999% of WAVs are 1 or 2 channels
+
+            // Get the frequency
+            Frequency = bytesToInt(wav, 24);
+
+            // Get past all the other sub chunks to get to the data subchunk:
+            var pos = 12; // First Subchunk ID from 12 to 16
+
+            // Keep iterating until we find the data chunk (i.e. 64 61 74 61 ...... (i.e. 100 97 116 97 in decimal))
+            while (!(wav[pos] == 100 && wav[pos + 1] == 97 && wav[pos + 2] == 116 && wav[pos + 3] == 97))
+            {
+                pos += 4;
+                var chunkSize = wav[pos] + wav[pos + 1] * 256 + wav[pos + 2] * 65536 + wav[pos + 3] * 16777216;
+                pos += 4 + chunkSize;
+            }
+
+            pos += 8;
+
+            // Pos is now positioned to start of actual sound data.
+            SampleCount = (wav.Length - pos) / 2; // 2 bytes per sample (16 bit sound mono)
+            if (ChannelCount == 2) SampleCount /= 2; // 4 bytes per sample (16 bit stereo)
+
+            // Allocate memory (right will be null if only mono sound)
+            LeftChannel = new float[SampleCount];
+            if (ChannelCount == 2) RightChannel = new float[SampleCount];
+            else RightChannel = null;
+
+            // Write to double array/s:
+            var i = 0;
+            while (pos < wav.Length)
+            {
+                LeftChannel[i] = bytesToFloat(wav[pos], wav[pos + 1]);
+                pos += 2;
+                if (ChannelCount == 2)
+                {
+                    RightChannel[i] = bytesToFloat(wav[pos], wav[pos + 1]);
+                    pos += 2;
+                }
+
+                i++;
+            }
+        }
+
+        // properties
+        public float[] LeftChannel { get; internal set; }
+        public float[] RightChannel { get; internal set; }
+        public int ChannelCount { get; internal set; }
+        public int SampleCount { get; internal set; }
+        public int Frequency { get; internal set; }
+
+        // convert two bytes to one float in the range -1 to 1
+        private static float bytesToFloat(byte firstByte, byte secondByte)
+        {
+            // convert two bytes to one short (little endian)
+            var s = (short) ((secondByte << 8) | firstByte);
+            // convert to range from -1 to (just below) 1
+            return s / 32768.0F;
+        }
+
+        private static int bytesToInt(byte[] bytes, int offset = 0)
+        {
+            var value = 0;
+            for (var i = 0; i < 4; i++) value |= bytes[offset + i] << (i * 8);
+            return value;
+        }
+
+        private static byte[] GetBytes(string filename)
+        {
+            return File.ReadAllBytes(filename);
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "[WAV: LeftChannel={0}, RightChannel={1}, ChannelCount={2}, SampleCount={3}, Frequency={4}]",
+                LeftChannel, RightChannel, ChannelCount, SampleCount, Frequency);
+        }
+    }
+}
