ba3bb6e2f28ad4e57491ce1521119bfeb39af2fa
molingyu <z1522716486@hotmail.com>
2019-08-24 11:13:20 +0800

    docs: add langFlag docs

--- Assets/Plugins/Rosetta/Runtime/LangFlag.cs
+++ Assets/Plugins/Rosetta/Runtime/LangFlag.cs
@@ -1,199 +1,766 @@
 ﻿namespace Rosetta.Runtime
 {
     /// <summary>
-    ///     Commonly used languages, the ISO 639-1 standard defines two-letter codes.Some flag additionally use the ISO 3166
-    ///     standard two character codes for countries and territories.
+    ///     Commonly used languages, the ISO 639-1 standard defines two-letter codes.Some flag additionally use 
+    ///     the ISO 3166 standard two character codes for countries and territories.
     /// </summary>
     public enum LangFlag
     {
+        /// <summary>
+        ///     Afar
+        /// </summary>
         AA,
+        /// <summary>
+        ///     Abkhazian
+        /// </summary>
         AB,
+        /// <summary>
+        ///     Avestan
+        /// </summary>
         AE,
+        /// <summary>
+        ///     Afrikaans
+        /// </summary>
         AF,
+        /// <summary>
+        ///     Akan
+        /// </summary>
         AK,
+        /// <summary>
+        ///     Amharic
+        /// </summary>
         AM,
+        /// <summary>
+        ///     Aragonese
+        /// </summary>
         AN,
+        /// <summary>
+        ///     Arabic
+        /// </summary>
         AR,
+        /// <summary>
+        ///     Assamese
+        /// </summary>
         AS,
+        /// <summary>
+        ///     Avaric
+        /// </summary>
         AV,
+        /// <summary>
+        ///     Aymara
+        /// </summary>
         AY,
+        /// <summary>
+        ///     Azerbaijani
+        /// </summary>
         AZ,
+        /// <summary>
+        ///     Bashkir
+        /// </summary>
         BA,
+        /// <summary>
+        ///     Belarusian
+        /// </summary>
         BE,
+        /// <summary>
+        ///     Bulgarian
+        /// </summary>
         BG,
+        /// <summary>
+        ///     Bihari
+        /// </summary>
         BH,
+        /// <summary>
+        ///     Bislama
+        /// </summary>
         BI,
+        /// <summary>
+        ///     Bambara
+        /// </summary>
         BM,
+        /// <summary>
+        ///     Bengali
+        /// </summary>
         BN,
+        /// <summary>
+        ///     Tibetan
+        /// </summary>
         BO,
+        /// <summary>
+        ///     Breton
+        /// </summary>
         BR,
+        /// <summary>
+        ///     Bosnian
+        /// </summary>
         BS,
+        /// <summary>
+        ///     Catalan
+        /// </summary>
         CA,
+        /// <summary>
+        ///     Chechen
+        /// </summary>
         CE,
+        /// <summary>
+        ///     Chamorro
+        /// </summary>
         CH,
+        /// <summary>
+        ///     Corsican
+        /// </summary>
         CO,
+        /// <summary>
+        ///     Cree
+        /// </summary>
         CR,
+        /// <summary>
+        ///     Czech
+        /// </summary>
         CS,
+        /// <summary>
+        ///     Church Slavic
+        /// </summary>
         CU,
+        /// <summary>
+        ///     Chuvash
+        /// </summary>
         CV,
+        /// <summary>
+        ///     Welsh
+        /// </summary>
         CY,
+        /// <summary>
+        ///     Danish
+        /// </summary>
         DA,
+        /// <summary>
+        ///     German
+        /// </summary>
         DE,
+        /// <summary>
+        ///     Divehi
+        /// </summary>
         DV,
+        /// <summary>
+        ///     Dzongkha
+        /// </summary>
         DZ,
+        /// <summary>
+        ///     Ewe
+        /// </summary>
         EE,
+        /// <summary>
+        ///     Greek
+        /// </summary>
         EL,
+        /// <summary>
+        ///     English
+        /// </summary>
         EN,
+        /// <summary>
+        ///     English(United Kingdom)
+        /// </summary>
         EN_GB,
+        /// <summary>
+        ///     Esperanto
+        /// </summary>
         EO,
+        /// <summary>
+        ///     Spanish
+        /// </summary>
         ES,
+        /// <summary>
+        ///     Estonian
+        /// </summary>
         ET,
+        /// <summary>
+        ///     Basque
+        /// </summary>
         EU,
+        /// <summary>
+        ///     Persian
+        /// </summary>
         FA,
+        /// <summary>
+        ///     Fulah
+        /// </summary>
         FF,
+        /// <summary>
+        ///     Finnish
+        /// </summary>
         FI,
+        /// <summary>
+        ///     Finnish
+        /// </summary>
         FJ,
+        /// <summary>
+        ///     Faroese
+        /// </summary>
         FO,
+        /// <summary>
+        ///     French
+        /// </summary>
         FR,
+        /// <summary>
+        ///    Western Frisian
+        /// </summary>
         FY,
+        /// <summary>
+        ///     Irish
+        /// </summary>
         GA,
+        /// <summary>
+        ///     Scottish Gaelic
+        /// </summary>
         GD,
+        /// <summary>
+        ///     Galician
+        /// </summary>
         GL,
+        /// <summary>
+        ///     Guaraní
+        /// </summary>
         GN,
+        /// <summary>
+        ///     Gujarati
+        /// </summary>
         GU,
+        /// <summary>
+        ///     Manx
+        /// </summary>
         GV,
+        /// <summary>
+        ///     Hausa
+        /// </summary>
         HA,
+        /// <summary>
+        ///     Hebrew
+        /// </summary>
         HE,
+        /// <summary>
+        ///     Hindi
+        /// </summary>
         HI,
+        /// <summary>
+        ///     Hiri Motu
+        /// </summary>
         HO,
+        /// <summary>
+        ///     Croatian
+        /// </summary>
         HR,
+        /// <summary>
+        ///     Haitian Creole
+        /// </summary>
         HT,
+        /// <summary>
+        ///     Hungarian
+        /// </summary>
         HU,
+        /// <summary>
+        ///     Armenian
+        /// </summary>
         HY,
+        /// <summary>
+        ///     Herero
+        /// </summary>
         HZ,
+        /// <summary>
+        ///     International Auxiliary Language Association
+        /// </summary>
         IA,
+        /// <summary>
+        ///     Indonesian
+        /// </summary>
         ID,
+        /// <summary>
+        ///     Interlingue
+        /// </summary>
         IE,
+        /// <summary>
+        ///     Igbo
+        /// </summary>
         IG,
+        /// <summary>
+        ///     Sichuan Yi
+        /// </summary>
         II,
+        /// <summary>
+        ///     Inupiaq
+        /// </summary>
         IK,
+        /// <summary>
+        ///     Ido
+        /// </summary>
         IO,
+        /// <summary>
+        ///     Icelandic
+        /// </summary>
         IS,
+        /// <summary>
+        ///     Italian
+        /// </summary>
         IT,
+        /// <summary>
+        ///     Inuktitut    
+        /// </summary>
         IU,
+        /// <summary>
+        ///     Japanese
+        /// </summary>
         JA,
+        /// <summary>
+        ///     Javanese
+        /// </summary>
         JV,
+        /// <summary>
+        ///     Georgian
+        /// </summary>
         KA,
+        /// <summary>
+        ///     Kongo
+        /// </summary>
         KG,
+        /// <summary>
+        ///     Kikuyu
+        /// </summary>
         KI,
+        /// <summary>
+        ///     Kwanyama
+        /// </summary>
         KJ,
+        /// <summary>
+        ///     Kazakh
+        /// </summary>
         KK,
+        /// <summary>
+        ///     Kalaallisut
+        /// </summary>
         KL,
+        /// <summary>
+        ///     Khmer
+        /// </summary>
         KM,
+        /// <summary>
+        ///     Kannada
+        /// </summary>
         KN,
+        /// <summary>
+        ///     Korean
+        /// </summary>
         KO_KR,
+        /// <summary>
+        ///     Korean(North Korea)
+        /// </summary>
         KO_KP,
+        /// <summary>
+        ///     Kanuri
+        /// </summary>
         KR,
+        /// <summary>
+        ///     Kashmiri
+        /// </summary>
         KS,
+        /// <summary>
+        ///     Kurdish
+        /// </summary>
         KU,
+        /// <summary>
+        ///     Komi
+        /// </summary>
         KV,
+        /// <summary>
+        ///     Cornish
+        /// </summary>
         KW,
+        /// <summary>
+        ///     Kirghiz
+        /// </summary>
         KY,
+        /// <summary>
+        ///     Latin
+        /// </summary>
         LA,
+        /// <summary>
+        ///     Luxembourgish
+        /// </summary>
         LB,
+        /// <summary>
+        ///     Ganda
+        /// </summary>
         LG,
+        /// <summary>
+        ///     Limburgish
+        /// </summary>
         LI,
+        /// <summary>
+        ///     Lingala
+        /// </summary>
         LN,
+        /// <summary>
+        ///     Lao
+        /// </summary>
         LO,
+        /// <summary>
+        ///     Lithuanian
+        /// </summary>
         LT,
+        /// <summary>
+        ///     Luba-Katanga
+        /// </summary>
         LU,
+        /// <summary>
+        ///     Latvian
+        /// </summary>
         LV,
+        /// <summary>
+        ///     Malagasy
+        /// </summary>
         MG,
+        /// <summary>
+        ///     Marshallese
+        /// </summary>
         MH,
+        /// <summary>
+        ///     Māori
+        /// </summary>
         MI,
+        /// <summary>
+        ///     Macedonian
+        /// </summary>
         MK,
+        /// <summary>
+        ///     Malayalam
+        /// </summary>
         ML,
+        /// <summary>
+        ///     Mongolian
+        /// </summary>
         MN,
+        /// <summary>
+        ///     Moldavian
+        /// </summary>
         MO,
+        /// <summary>
+        ///     Marathi
+        /// </summary>
         MR,
+        /// <summary>
+        ///     Malay
+        /// </summary>
         MS,
+        /// <summary>
+        ///     Maltese
+        /// </summary>
         MT,
+        /// <summary>
+        ///     Burmese
+        /// </summary>
         MY,
+        /// <summary>
+        ///     Nauru
+        /// </summary>
         NA,
+        /// <summary>
+        ///     Norwegian 
+        /// </summary>
         NB,
+        /// <summary>
+        ///     North Ndebele
+        /// </summary>
         ND,
+        /// <summary>
+        ///     Nepali
+        /// </summary>
         NE,
+        /// <summary>
+        ///     Ndonga
+        /// </summary>
         NG,
+        /// <summary>
+        ///     Dutch
+        /// </summary>
         NL,
+        /// <summary>
+        ///     Norwegian 
+        /// </summary>
         NN,
+        /// <summary>
+        ///     Norwegian
+        /// </summary>
         NO,
+        /// <summary>
+        ///     South Ndebele
+        /// </summary>
         NR,
+        /// <summary>
+        ///     Navajo
+        /// </summary>
         NV,
+        /// <summary>
+        ///     Chichewa
+        /// </summary>
         NY,
+        /// <summary>
+        ///     Occitan
+        /// </summary>
         OC,
+        /// <summary>
+        ///     Ojibwa
+        /// </summary>
         OJ,
+        /// <summary>
+        ///     Oromo
+        /// </summary>
         OM,
+        /// <summary>
+        ///     Oriya
+        /// </summary>
         OR,
+        /// <summary>
+        ///     Ossetian
+        /// </summary>
         OS,
+        /// <summary>
+        ///     Panjabi
+        /// </summary>
         PA,
+        /// <summary>
+        ///     Pāli
+        /// </summary>
         PI,
+        /// <summary>
+        ///     Polish
+        /// </summary>
         PL,
+        /// <summary>
+        ///     Pashto
+        /// </summary>
         PS,
+        /// <summary>
+        ///     Portuguese
+        /// </summary>
         PT,
+        /// <summary>
+        ///     Portuguese(Brazil)
+        /// </summary>
         PT_BR,
+        /// <summary>
+        ///     Quechua
+        /// </summary>
         QU,
+        /// <summary>
+        ///     Raeto-Romance
+        /// </summary>
         RM,
+        /// <summary>
+        ///     Kirundi
+        /// </summary>
         RN,
+        /// <summary>
+        ///     Romanian
+        /// </summary>
         RO,
+        /// <summary>
+        ///     Russian
+        /// </summary>
         RU,
+        /// <summary>
+        ///     Kinyarwanda
+        /// </summary>
         RW,
+        /// <summary>
+        ///     Sanskrit
+        /// </summary>
         SA,
+        /// <summary>
+        ///     Sardinian
+        /// </summary>
         SC,
+        /// <summary>
+        ///     Sindhi
+        /// </summary>
         SD,
+        /// <summary>
+        ///     Northern Sami
+        /// </summary>
         SE,
+        /// <summary>
+        ///     Sango
+        /// </summary>
         SG,
+        /// <summary>
+        ///     Sinhalese
+        /// </summary>
         SI,
+        /// <summary>
+        ///     Slovak
+        /// </summary>
         SK,
+        /// <summary>
+        ///     Slovenian
+        /// </summary>
         SL,
+        /// <summary>
+        ///     Samoan
+        /// </summary>
         SM,
+        /// <summary>
+        ///     Shona
+        /// </summary>
         SN,
+        /// <summary>
+        ///     Somali
+        /// </summary>
         SO,
+        /// <summary>
+        ///     Albanian
+        /// </summary>
         SQ,
+        /// <summary>
+        ///     Serbian
+        /// </summary>
         SR,
+        /// <summary>
+        ///     Swati
+        /// </summary>
         SS,
+        /// <summary>
+        ///     Sotho
+        /// </summary>
         ST,
+        /// <summary>
+        ///     Sundanese
+        /// </summary>
         SU,
+        /// <summary>
+        ///     Swedish
+        /// </summary>
         SV,
+        /// <summary>
+        ///     Swahili
+        /// </summary>
         SW,
+        /// <summary>
+        ///     Tamil
+        /// </summary>
         TA,
+        /// <summary>
+        ///     Telugu
+        /// </summary>
         TE,
+        /// <summary>
+        ///     Tajik
+        /// </summary>
         TG,
+        /// <summary>
+        ///     Thai
+        /// </summary>
         TH,
+        /// <summary>
+        ///     Tigrinya
+        /// </summary>
         TI,
+        /// <summary>
+        ///     Turkmen
+        /// </summary>
         TK,
+        /// <summary>
+        ///     Tagalog
+        /// </summary>
         TL,
+        /// <summary>
+        ///     Tswana
+        /// </summary>
         TN,
+        /// <summary>
+        ///     Tonga
+        /// </summary>
         TO,
+        /// <summary>
+        ///     Turkish
+        /// </summary>
         TR,
+        /// <summary>
+        ///     Tsonga
+        /// </summary>
         TS,
+        /// <summary>
+        ///     Tatar
+        /// </summary>
         TT,
+        /// <summary>
+        ///     Twi
+        /// </summary>
         TW,
+        /// <summary>
+        ///     Tahitian
+        /// </summary>
         TY,
+        /// <summary>
+        ///     Uyghur
+        /// </summary>
         UG,
+        /// <summary>
+        ///     Ukrainian
+        /// </summary>
         UK,
+        /// <summary>
+        ///     Urdu
+        /// </summary>
         UR,
+        /// <summary>
+        ///     Uzbek
+        /// </summary>
         UZ,
+        /// <summary>
+        ///     Venda
+        /// </summary>
         VE,
+        /// <summary>
+        ///     Vietnamese
+        /// </summary>
         VI,
+        /// <summary>
+        ///     Volapük
+        /// </summary>
         VO,
+        /// <summary>
+        ///     Walloon
+        /// </summary>
         WA,
+        /// <summary>
+        ///     Wolof
+        /// </summary>
         WO,
+        /// <summary>
+        ///     Xhosa
+        /// </summary>
         XH,
+        /// <summary>
+        ///     Yiddish
+        /// </summary>
         YI,
+        /// <summary>
+        ///     Yoruba
+        /// </summary>
         YO,
+        /// <summary>
+        ///     Zhuang
+        /// </summary>
         ZA,
+        /// <summary>
+        ///     Simplified Chinese
+        /// </summary>
         ZH_CN,
+        /// <summary>
+        ///     Traditional Chinese
+        /// </summary>
         ZH_TW,
+        /// <summary>
+        ///     Zulu
+        /// </summary>
         ZU
     }
 }
