754b6856ceb3870f41246eeedea432754d21781a
molingyu <z1522716486@hotmail.com>
2019-08-24 11:18:03 +0800

    refactor: update RuntimeSetting langFlag ui

--- Assets/Plugins/Rosetta/Runtime/RosettaRuntimeSetting.cs
+++ Assets/Plugins/Rosetta/Runtime/RosettaRuntimeSetting.cs
@@ -2,27 +2,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEditor;
 using UnityEngine;
 
 namespace Rosetta.Runtime
 {
-    [CreateAssetMenu(fileName = "RuntimeSetting", menuName = "Rosetta/RuntimeSetting")]
+    [CreateAssetMenu(fileName = "RosettaRuntimeSetting", menuName = "Rosetta/RuntimeSetting")]
     [Serializable]
     public class RosettaRuntimeSetting : SerializedScriptableObject
     {
         public List<string> DefaultI18NSpaces = new List<string> { "UI" };
 
-        [OnValueChanged("SwitchDevLocale")] public LangFlag DevLocale = LangFlag.EN;
+        [ValueDropdown("langFlag")]
+        [OnValueChanged("SwitchDevLocale")]
+        public LangFlag DevLocale = LangFlag.EN;
 
-        [HideInInspector] public LangFlag? Locale = null;
+        [HideInInspector] public LangFlag Locale = LangFlag.EN;
 
-        [ValueDropdown("_textFileType")] public I18NFileType TextFileType;
+        [ValueDropdown("textFileType")] public I18NFileType TextFileType;
 
 #if UNITY_EDITOR
         private LangFlag _devLocale;
 
-        private static IEnumerable _textFileType = new ValueDropdownList<I18NFileType>
+        private static IEnumerable langFlag()
+        {
+            var list = new ValueDropdownList<LangFlag>();
+            Rosetta.LangNames.ForEach(pair => list.Add(new ValueDropdownItem<LangFlag>($"{pair.Value} ({pair.Key.ToString()})", pair.Key)));
+            return list;
+        }
+        
+        private static IEnumerable textFileType = new ValueDropdownList<I18NFileType>
         {
             {"Po", I18NFileType.Po}
             //{"Mo", I18NFileType.Mo},
