750ea6d5dfb71c847977c473f13bc070713e25f6
molingyu <z1522716486@hotmail.com>
2019-08-24 22:04:53 +0800

    feat: add SceneCollector

---
+++ Assets/Plugins/Rosetta/Editor/Collector/SceneCollector.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+
+namespace Rosetta.Editor.Collector
+{
+    [Serializable]
+    public class SceneCollector : PrefabCollectorBase
+    {
+        public List<SceneAsset> SceneList = new List<SceneAsset>();
+
+        public override void Collect(string space)
+        {
+            EditorSceneManager.SaveOpenScenes();
+            SceneList.ForEach(sceneAsset =>
+            {
+                var scene = EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(sceneAsset), OpenSceneMode.Single);
+                GameObjects = scene.GetRootGameObjects().ToList();
+                PrefixName = $"Scene[{scene.name}]:";
+                base.Collect(space);
+            });
+        }
+    }
+}    
