fac5ff63a32347915d18a9325c404618e1ee4a06
molingyu <z1522716486@hotmail.com>
2019-08-24 22:18:24 +0800

    test: init Rosetta Editor Test

---
+++ Assets/Plugins/Rosetta/Editor/Tests/CreatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Rosetta.Editor.Tests
+{
+    public class CreatorTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CreatorTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator CreatorTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
