e5efdc68548e41b45c79796fb6f3061755ecb1d6
molingyu <z1522716486@hotmail.com>
2019-08-24 11:06:00 +0800

    refactor: Add space support to the load method.

--- Assets/Plugins/Rosetta/Runtime/Loader/LoaderBase.cs
+++ Assets/Plugins/Rosetta/Runtime/Loader/LoaderBase.cs
@@ -30,10 +30,11 @@ namespace Rosetta.Runtime.Loader
         /// Load i18n file by LangFlag.
         /// </summary>
         /// <typeparam name="T">The file load type(string or binary)</typeparam>
+        /// <param name="space">the file space of the to be loaded</param>
         /// <param name="filename">The file name of the file to be loaded.</param>
         /// <param name="flag">The LangFlag of the file to be loaded.</param>
         /// <returns></returns>
-        public abstract T Load<T>(string filename, LangFlag? flag = null);
+        public abstract T Load<T>(string space, string filename, LangFlag? flag = null);
 
         /// <summary>
         ///     Loading i18n files will be loaded in two ways: binary or string
