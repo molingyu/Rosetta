8559fb4327499d6d92ae7b07b4a1555a0ba189aa
molingyu <z1522716486@hotmail.com>
2019-08-24 17:02:51 +0800

    feat: add I18N TMP text component

---
+++ Assets/Plugins/Rosetta/Runtime/Component/I18NTMPText.cs
@@ -0,0 +1,40 @@
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace Rosetta.Runtime.Component
+{
+    /// <summary>
+    ///     Marking the current TMPText component requires I18N.
+    /// </summary>
+    [AddComponentMenu("Rosetta/I18NTMPText")]
+    [RequireComponent(typeof(TMP_Text))]
+    [HelpURL("https://molingyu.github.io/Rosetta/guides/makeI18NRes/component/I18NTMPText.html")]
+    public class I18NTMPText : I18NComponentBase
+    {
+        private TMP_FontAsset _devFont;
+        private string _devString;
+        /// <summary>
+        ///      If true, the corresponding i18n font is loaded via the `VirtualFontName` instead of the currently set font.
+        /// </summary>
+        public bool IsVirtualFont;
+        /// <summary>
+        ///     I18n font name.
+        /// </summary>
+        [ShowIf("IsVirtualFont")] public string VirtualFontName = "";
+
+        private void Start()
+        {
+            _devString = gameObject.GetComponent<TMP_Text>().text;
+            _devFont = gameObject.GetComponent<TMP_Text>().font;
+            Create();
+        }
+
+        public override void Refresh()
+        {
+            var text = gameObject.GetComponent<TMP_Text>();
+            text.text = Rosetta.GetText(_devString, I18NSpace);
+            if (IsVirtualFont) text.font = Rosetta.IsDefault() ? _devFont : Rosetta.GetTMP_Font(VirtualFontName);
+        }
+    }
+}
