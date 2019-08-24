d4f3c750b333c413cad30242609021b74b80d24b
molingyu <z1522716486@hotmail.com>
2019-08-24 17:01:14 +0800

    feat: add I18N audio component

---
+++ Assets/Plugins/Rosetta/Runtime/Component/I18NAudio.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Rosetta.Runtime.Component
+{
+    /// <summary>
+    ///     Marking the current Audio component requires I18N.
+    /// </summary>
+    [AddComponentMenu("Rosetta/I18NAudio")]
+    [RequireComponent(typeof(AudioSource))]
+    [HelpURL("https://molingyu.github.io/Rosetta/guides/makeI18NRes/component/I18NAudio.html")]
+    public class I18NAudio : I18NComponentBase
+    {
+        private AudioClip _devAudio;
+        /// <summary>
+        ///     
+        /// </summary>
+        public string ResName;
+
+        private void Start()
+        {
+            _devAudio = gameObject.GetComponent<AudioSource>().clip;
+            Create();
+        }
+
+        public override void Refresh()
+        {
+            var audioSource = gameObject.GetComponent<AudioSource>();
+            var status = audioSource.isPlaying;
+            audioSource.clip = Rosetta.IsDefault() ? _devAudio : Rosetta.GetAudio(ResName, I18NSpace);
+            if (status) audioSource.Play();
+        }
+    }
+}
