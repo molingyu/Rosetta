6e4e3d4a161f57c462ef10ee86d9119f1e88b399
molingyu <z1522716486@hotmail.com>
2019-08-24 17:02:00 +0800

    feat: add I18N sprite component

---
+++ Assets/Plugins/Rosetta/Runtime/Component/I18NSpriteRender.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Rosetta.Runtime.Component
+{
+    /// <summary>
+    ///     Marking the current SpriteRenderer component requires I18N.
+    /// </summary>
+    [AddComponentMenu("Rosetta/I18NSpriteRenderer")]
+    [RequireComponent(typeof(SpriteRenderer))]
+    [HelpURL("https://molingyu.github.io/Rosetta/guides/makeI18NRes/component/I18NSpriteRenderer.html")]
+    public class I18NSpriteRenderer : I18NComponentBase
+    {
+        private Sprite _devSprite;
+        /// <summary>
+        ///     I18N image name.
+        /// </summary>
+        public string ResName;
+
+        private void Start()
+        {
+            _devSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+            Create();
+        }
+
+        public override void Refresh()
+        {
+            var image = gameObject.GetComponent<SpriteRenderer>();
+            image.sprite = Rosetta.IsDefault() ? _devSprite : Rosetta.GetSprite(ResName, I18NSpace);
+        }
+    }
+}
